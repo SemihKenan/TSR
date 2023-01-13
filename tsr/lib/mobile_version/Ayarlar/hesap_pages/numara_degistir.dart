eefa2caf0b78d92fff2dd64677791ea457dea04f
Suleysi <115027565+Suleysi@users.noreply.github.com>
2023-01-13 19:31:51 +0300

    Whatsapp_Clone(13.01.2023)

--- tsr/lib/mobile_version/Ayarlar/hesap_pages/numara_degistir.dart
+++ tsr/lib/mobile_version/Ayarlar/hesap_pages/numara_degistir.dart
@@ -27,7 +27,50 @@ class _numaradegisState extends State<numaradegis> {
           },
           icon: const Icon(Icons.arrow_back),
         ),
-        title: const Text('Numara Deüiştir'),
+        title: const Text('Numara Değiştir'),
+      ),
+      body: Column(
+        crossAxisAlignment: CrossAxisAlignment.start,
+        children: [
+          const SizedBox(
+            height: 10,
+          ),
+          Container(
+            alignment: Alignment.center,
+            child: const Image(
+              image: AssetImage('images/phone.png'),
+            ),
+          ),
+          const Padding(
+            padding: EdgeInsets.all(10),
+            child: Text(
+                'Telefon numaranızı değiştiriğinizde hesap bilgileriniz, gruplarınız ve ayarlarınız taşınır.'),
+          ),
+          const Padding(
+            padding: EdgeInsets.all(10.0),
+            child: Text(
+                'Devam etmeden önce lütfen yeni numaranızdan SMS ve arama alabildiğinize emin olun'),
+          ),
+          const Padding(
+            padding: EdgeInsets.all(10.0),
+            child: Text(
+                'Eğer hem telefoonunuz hem de numaranız yeniyse lütfen öncelikle eski telefonunuzdan numaranızı değiştirin.'),
+          ),
+          Expanded(
+              child: Container(
+            alignment: Alignment.bottomCenter,
+            child: Container(
+              width: 70,
+              height: 25,
+              decoration: BoxDecoration(
+                  borderRadius: BorderRadius.circular(20), color: Colors.green),
+              child: const Text(
+                'İleri',
+                textAlign: TextAlign.center,
+              ),
+            ),
+          ))
+        ],
       ),
     );
   }
