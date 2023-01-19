85f253a55bb91a52e351dd7d2b762ce80196c3a7
Suleysi <115027565+Suleysi@users.noreply.github.com>
2023-01-19 21:12:24 +0300

    19.01.2023

--- tsr/lib/main.dart
+++ tsr/lib/main.dart
@@ -1,10 +1,5 @@
 import 'package:flutter/material.dart';
-import 'package:tsr/Desktop/Gizlilik/Gizlilik.dart';
-
 import 'package:tsr/Desktop/Screens/HomePage.dart';
-import 'package:tsr/Desktop/Screens/Status.dart';
-import 'package:tsr/mobile_version/Ayarlar/Gizlilik_mobile/gizlilik_mobil.dart';
-import 'package:tsr/mobile_version/Screens/homepage_mobile.dart';
 
 void main() {
   runApp(const MyApp());
@@ -12,19 +7,15 @@ void main() {
 
 class MyApp extends StatelessWidget {
   const MyApp({super.key});
-
-  // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
-    //final ekran = MediaQuery.of(context).size.width;
     return MaterialApp(
       debugShowCheckedModeBanner: false,
       title: 'Flutter Demo',
       theme: ThemeData(
         primarySwatch: Colors.green,
       ),
-      home: HomePage(),
-      //home: ekran < 500 ? const homepage_mobile() : HomePage(),
+      home: const HomePage(),
     );
   }
 }
